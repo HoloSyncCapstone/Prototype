e3c25275cedc7b83e12e9d93b62e434842706496
sooraj-gupta <soorajgupta04@gmail.com>
2025-09-29 17:55:46 -0500

    Initial Commit

---
+++ Prototype/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Prototype
+//
+//  Created by Patron on 9/29/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
---
+++ Prototype/PrototypeApp.swift
@@ -0,0 +1,33 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Patron on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
